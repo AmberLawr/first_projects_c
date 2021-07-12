a81a03e8f6e4bc1faac436a71438584d6e95d5e7
AmberLawr <80423210+AmberLawr@users.noreply.github.com>
2021-07-12 16:17:25 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -3,16 +3,17 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# define MAX_FD 5050
 
-
-int		get_next_line(int fd, char **line);
+char	*get_next_line(int fd);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memchr_modified(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *s, int c);
 
 #endif
