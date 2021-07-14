b5d6ccf7e9b914990b6ee21f2a9b0d69cc78556d
AmberLawr <80423210+AmberLawr@users.noreply.github.com>
2021-07-14 19:45:08 +0200

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,5 +1,5 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -10,10 +10,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strdup(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-char	*ft_strchr(const char *s, int c);
 
 #endif
